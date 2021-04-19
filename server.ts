2bbbd98aa7026f647d28e52db639247e9595dfff
Yash Karandikar <nerdstep710@gmail.com>
2021-04-19 14:53:47 -0500

    Handle CORS

--- server.ts
+++ server.ts
@@ -2,7 +2,12 @@ import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 
 const httpServer = createServer();
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on("connection", (socket: Socket) => {
     socket.emit("hello", "world");
